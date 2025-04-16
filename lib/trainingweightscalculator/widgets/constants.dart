ffab571a7405535dcebf9a9a0b765c0c157f72a2
Gigiuz <sassaroluigi@gmail.com>
2025-04-16 18:40:11 +0200

    feat: new const error message

--- lib/trainingweightscalculator/widgets/constants.dart
+++ lib/trainingweightscalculator/widgets/constants.dart
@@ -19,6 +19,7 @@ class AppConstants {
   /// Form error messages.
   static const String errorSelectTrainingType = 'Seleziona un tipo di allenamento';
   static const String errorEnterWeight = 'Inserisci il peso';
+   static const String errorEnterBodyWeight = 'Inserisci il peso corporeo';
   static const String errorEnterValidNumber = 'Inserisci un numero valido';
   static const String errorSelectRepetitions = 'Seleziona il numero di ripetizioni';
 }
