efe5cf315625c929ffba54804d00d824edf6db91
Gigiuz <sassaroluigi@gmail.com>
2025-04-18 10:47:05 +0200

    feat: added 1RM streetlifting calculate

--- lib/trainingweightscalculator/widgets/weight_calculation_service.dart
+++ lib/trainingweightscalculator/widgets/weight_calculation_service.dart
@@ -8,21 +8,31 @@ class WeightCalculationService {
   /// Returns a map of percentage labels to calculated weights.
   static Map<String, double> calculateTrainingWeights({
     required double weight,
-    required String rmType,
+    required String rmType, 
+    required bool isStreetlifting, required double bodyWweight,
   }) {
     final Map<String, double> results = {};
     
     // Calculate based on the RM type
     switch (rmType) {
       case '1RM':
-        // Direct calculation from 1RM
+      if(isStreetlifting){
+        double streetliftingCalculateFactor = bodyWweight + weight;
+        results['60%'] = (((streetliftingCalculateFactor * 0.6) - bodyWweight) + (weight * 0.6)) / 2;
+        results['70%'] = (((streetliftingCalculateFactor * 0.7) - bodyWweight) + (weight * 0.7)) / 2;
+        results['80%'] = (((streetliftingCalculateFactor * 0.8) - bodyWweight) + (weight * 0.8)) / 2;
+        results['90%'] = (((streetliftingCalculateFactor * 0.9) - bodyWweight) + (weight * 0.9)) / 2;
+        results['95%'] = (((streetliftingCalculateFactor * 0.95) - bodyWweight) + (weight * 0.95)) / 2;
+        break;
+      } else {
         results['60%'] = weight * 0.6;
         results['70%'] = weight * 0.7;
         results['80%'] = weight * 0.8;
         results['90%'] = weight * 0.9;
         results['95%'] = weight * 0.95;
         break;
-        
+      }
+              
       case '5RM':
         // Formula to estimate 1RM from 5RM: weight * 1.15
         final estimated1RM = weight * 1.15;
